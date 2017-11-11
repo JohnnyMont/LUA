c73b75d57ecf320ae46ddf8bbb4b87d339893bce
JohnnyMont <jonathan2011monteiro@hotmail.com>
2017-11-11 16:46:08 -0200

    Add files via upload

---
+++ operacaoes.lua
@@ -0,0 +1,20 @@
+-- Basicas
+io.write("Soma: ",5+3, "\n")
+io.write("Sub: ",5-3, "\n")
+io.write("Multi: ",5*3, "\n")
+io.write("Div: ",5/3, "\n")
+io.write("Modulo: ",5%3, "\n")
+-- Com variaveis
+numeroA = 10
+numeroB = 20
+resultado = numeroA + numeroB
+io.write("Soma: ",resultado, "\n")
+resultado = numeroA * numeroB
+io.write("Multi: ",resultado, "\n")
+-- Potencia
+-- 1ª numero Base
+-- 2ª numero numeromelevado
+io.write("Potencia: ",math.pow(2,4), "\n")
+-- Raiz Quadrada
+-- Valor
+io.write("Raiz: ",math.sqrt(9), "\n")
